497616132086d98d66e3716c5993143215eac1c9
tmjii <triaji20@gmail.com>
2026-02-09 18:12:30 +0700

    update dashboard

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -5,100 +5,15 @@ Dashboard
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
-<!-- Content Header -->
-<div class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0">Dashboard</h1>
-            </div>
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Dashboard</li>
-                </ol>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Main content -->
-<section class="content">
-    <div class="container-fluid">
-        <?php if (session()->getFlashdata('success')): ?>
-            <div class="alert alert-success alert-dismissible fade show">
-                <?= session()->getFlashdata('success') ?>
-                <button type="button" class="close" data-dismiss="alert">&times;</button>
-            </div>
-        <?php endif; ?>
-        
-        <!-- Info boxes -->
-        <div class="row">
-            <div class="col-12 col-sm-6 col-md-3">
-                <div class="info-box">
-                    <span class="info-box-icon bg-info elevation-1"><i class="fas fa-users"></i></span>
-                    <div class="info-box-content">
-                        <span class="info-box-text">Users</span>
-                        <span class="info-box-number">1,410</span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-sm-6 col-md-3">
-                <div class="info-box mb-3">
-                    <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
-                    <div class="info-box-content">
-                        <span class="info-box-text">Sales</span>
-                        <span class="info-box-number">410</span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-sm-6 col-md-3">
-                <div class="info-box mb-3">
-                    <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-chart-line"></i></span>
-                    <div class="info-box-content">
-                        <span class="info-box-text">Revenue</span>
-                        <span class="info-box-number">$13,648</span>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-sm-6 col-md-3">
-                <div class="info-box mb-3">
-                    <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-heart"></i></span>
-                    <div class="info-box-content">
-                        <span class="info-box-text">Likes</span>
-                        <span class="info-box-number">93,139</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Main row -->
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="card">
-                    <div class="card-header border-0">
-                        <h3 class="card-title">Welcome to AdminLTE</h3>
-                    </div>
-                    <div class="card-body">
-                        <p>This is a CodeIgniter 4 application integrated with AdminLTE 3 template.</p>
-                        <p>You can customize this dashboard according to your needs.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="card">
-                    <div class="card-header border-0">
-                        <h3 class="card-title">Quick Links</h3>
-                    </div>
-                    <div class="card-body">
-                        <ul>
-                            <li><a href="https://adminlte.io/docs/3.2/" target="_blank">AdminLTE Documentation</a></li>
-                            <li><a href="https://codeigniter.com/user_guide/" target="_blank">CodeIgniter 4 Documentation</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+<?= view('dashboard_content', [
+    'total_pasien' => $total_pasien ?? 0,
+    'total_pendaftaran' => $total_pendaftaran ?? 0,
+    'total_kunjungan' => $total_kunjungan ?? 0,
+    'total_asesmen' => $total_asesmen ?? 0,
+    'total_diagnosa' => $total_diagnosa ?? 0,
+    'total_users' => $total_users ?? 0,
+    'recent_pasien' => $recent_pasien ?? [],
+    'recent_pendaftaran' => $recent_pendaftaran ?? [],
+    'recent_kunjungan' => $recent_kunjungan ?? []
+]) ?>
 <?= $this->endSection() ?>
